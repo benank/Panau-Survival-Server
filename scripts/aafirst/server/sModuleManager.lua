4ba389309232bff31dcb683af5c75f523078fae7
benank <mp.paradigm@gmail.com>
2020-02-09 00:32:05 -0800

    Unload testing modules when server starts

---
+++ scripts/aafirst/server/sModuleManager.lua
@@ -0,0 +1,6 @@
+-- Unload some modules only for testing
+Console:Run("unload editor")
+Console:Run("unload effects")
+Console:Run("unload LocalizedWeather")
+Console:Run("unload modelviewer")
+Console:Run("unload spn")
