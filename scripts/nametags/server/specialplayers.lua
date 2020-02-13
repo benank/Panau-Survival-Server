4433ce5b968ff307bba89dfb095de4e61e9eb8ce
benank <mp.paradigm@gmail.com>
2020-02-12 22:59:32 -0800

    Add Rexun as staff

--- scripts/nametags/server/specialplayers.lua
+++ scripts/nametags/server/specialplayers.lua
@@ -4,6 +4,7 @@ spcol["[Admin]"] = Color(255,0,0)
 spcol["[Donator]"] = Color(164,95,204)
 
 sp["STEAM_0:1:31147722"] = "[Staff]" -- Lord_Farquaad
+sp["STEAM_0:0:47613644"] = "[Staff]" -- Rexun
 
 
 
