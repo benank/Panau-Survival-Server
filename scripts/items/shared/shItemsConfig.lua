fc4e3dbc56a1759d02ad337dfd74c46cf21f0100
benank <mp.paradigm@gmail.com>
2020-02-27 11:33:42 -0800

    Adjust Bandages health restore amount

--- scripts/items/shared/shItemsConfig.lua
+++ scripts/items/shared/shItemsConfig.lua
@@ -3,7 +3,7 @@ ItemsConfig =
     usables = -- No food items here because those have no delay when using them
     {
         ["Mine"] = {use_time = 3},
-        ["Bandages"] = {restore_hp = 0.1, use_time = 3},
+        ["Bandages"] = {restore_hp = 0.2, use_time = 3},
         ["Healthpack"] = {restore_hp = 0.5, use_time = 5},
         ["Woet"] = {use_time = 2},
 
