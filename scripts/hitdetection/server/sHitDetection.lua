6b094896ba72c6baa5f433332722c9b48178ae1d
benank <mp.paradigm@gmail.com>
2020-02-29 15:50:44 -0800

    Add more checks

--- scripts/hitdetection/server/sHitDetection.lua
+++ scripts/hitdetection/server/sHitDetection.lua
@@ -7,6 +7,9 @@ end
 
 function sHitDetection:BulletHit(args, player)
     if not args.bone or not BoneModifiers[args.bone.name] then return end
+    if not IsValid(args.attacker) then return end
+
+    if args.attacker:GetValue("InSafezone") or player:GetValue("InSafezone") then return end
 
     local weapon = args.attacker:GetEquippedWeapon()
     if not weapon then return end
@@ -26,14 +29,23 @@ function sHitDetection:BulletHit(args, player)
                 armor_name = armor_name,
                 damage = original_damage
             })
-            
+
         end
     end
 
+    local old_hp = player:GetHealth()
     player:Damage(damage / 100, DamageEntity.Bullet, args.attacker)
 
     print(string.format("%s shot %s for %s damage [%s]",
     args.attacker:GetName(), player:GetName(), tostring(damage), tostring(weapon.id)))
+
+    -- If their health doesn't change after being shot
+    Timer.SetTimeout(200, function()
+        if IsValid(player) and player:GetHealth() >= old_hp and damage > 0 then
+            print(player:GetName() .. " kicked for health hacks")
+            player:Kick("Health hacks")
+        end
+    end)
 
 end
 
