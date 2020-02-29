288ffe45970d4968f7eb9e59d65c828193fed991
benank <mp.paradigm@gmail.com>
2020-02-28 18:56:39 -0800

    Update nametags script a bit

--- scripts/nametags/server/specialplayers.lua
+++ scripts/nametags/server/specialplayers.lua
@@ -1,11 +1,6 @@
 sp = {}
 spcol = {}
-spcol["[Admin]"] = Color(255,0,0)
-spcol["[Donator]"] = Color(164,95,204)
+spcol["Staff"] = Color(14,242,124)
 
-sp["STEAM_0:1:31147722"] = "[Staff]" -- Lord_Farquaad
-sp["STEAM_0:0:47613644"] = "[Staff]" -- Rexun
-
-
-
-sp["STEAM_0:0:53272641"] = "[Donator]" -- kpina
+sp["STEAM_0:1:31147722"] = "Staff" -- Lord_Farquaad
+sp["STEAM_0:0:47613644"] = "Staff" -- Rexun
