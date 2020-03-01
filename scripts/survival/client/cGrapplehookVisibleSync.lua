1c6b3d10cdc90ccee155eb2c24bd03493f0ce987
benank <mp.paradigm@gmail.com>
2020-03-01 15:18:10 -0800

    Fix grapple sync error

--- scripts/survival/client/cGrapplehookVisibleSync.lua
+++ scripts/survival/client/cGrapplehookVisibleSync.lua
@@ -1,18 +1,25 @@
 function DrawGrapples()
 
 	for player in Client:GetStreamedPlayers() do
-	
-		local state = player:GetBaseState()
-		local lastate = player:GetLeftArmState()
-		
-		if state == AnimationState.SReelFlight 
-		or lastate == AnimationState.LaSGrapple then
-			Render:DrawLine(
-				player:GetBonePosition("ragdoll_LeftHand"),
-				player:GetAimTarget().position,
-				Color(60, 60, 60, 255 * math.max(0, 1 - (Vector3.Distance(player:GetPosition(), Camera:GetPosition()) / 1024)))
-			)
-		end
+    
+        if IsValid(player) 
+        and player:GetBonePosition("ragdoll_LeftHand") 
+        and player:GetAimTarget() and player:GetAimTarget().position
+        and player:GetPosition() then
+
+            local state = player:GetBaseState()
+            local lastate = player:GetLeftArmState()
+            
+            if state == AnimationState.SReelFlight 
+            or lastate == AnimationState.LaSGrapple then
+                Render:DrawLine(
+                    player:GetBonePosition("ragdoll_LeftHand"),
+                    player:GetAimTarget().position,
+                    Color(60, 60, 60, 255 * math.max(0, 1 - (Vector3.Distance(player:GetPosition(), Camera:GetPosition()) / 1024)))
+                )
+            end
+
+        end
 		
 	end
 	
