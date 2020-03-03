bc5a4c58da337d58f848eaa800f43833e4a4e2b0
benank <mp.paradigm@gmail.com>
2020-03-02 17:09:55 -0800

    Increase inventory ping limit

--- scripts/inventory/server/sInventory.lua
+++ scripts/inventory/server/sInventory.lua
@@ -333,7 +333,7 @@ function sInventory:CanPlayerPerformOperations(player)
     -- eventually modify this so that admins can do stuff if they clone the inv
     return IsValid(player) and IsValid(self.player) and player == self.player and self.operation_block == 0
         and not player:GetValue("Loading") and player:GetEnabled() and player:GetValue("InventoryOperationBlock") == 0
-        and player:GetPing() < 1000
+        and player:GetPing() < 1500
 
 end
 
