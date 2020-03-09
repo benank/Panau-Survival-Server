f5d197f5e62208c3192a34b4e20c507505025208
benank <mp.paradigm@gmail.com>
2020-03-09 15:16:44 -0700

    Fix police vest not working properly

--- scripts/items/shared/shItemsConfig.lua
+++ scripts/items/shared/shItemsConfig.lua
@@ -32,6 +32,7 @@ ItemsConfig =
             ["Helmet"] = {dura_per_hit = 6},
             ["Police Helmet"] = {dura_per_hit = 4},
             ["Military Helmet"] = {dura_per_hit = 2},
+            ["Police Vest"] = {dura_per_hit = 6},
             ["Military Vest"] = {dura_per_hit = 4},
             ["Kevlar Vest"] = {dura_per_hit = 2}
         },
