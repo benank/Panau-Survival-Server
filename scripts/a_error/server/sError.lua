a1cd562ef78db5403544ba364d0eadbe04ed9fcb
benank <terkolskie@gmail.com>
2020-04-27 18:04:55 -0700

    Fix typo

--- scripts/a_error/server/sError.lua
+++ scripts/a_error/server/sError.lua
@@ -1,19 +1,15 @@
 Events:Subscribe("ModuleError", function(e)
     local error = FormatError(e, "SERVER ERROR")
 
-    print("SERVER EROR")
-    print(error)
     Events:Fire("Discord", {
-        channel = "Error",
+        channel = "Errors",
         content = error
     })
 end)
 
 Network:Subscribe("ModuleError", function(args, player)
-    print("CLIENT ERROR")
-    print(args.error)
     Events:Fire("Discord", {
-        channel = "Error",
+        channel = "Errors",
         content = tostring(args.error)
     })
 end)
