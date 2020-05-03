84071306ee64857cfa00a3f8e35eef401c87ecd6
benank <terkolskie@gmail.com>
2020-05-03 11:56:50 -0700

    Remove modulechecker

--- scripts/ModuleChecker/server/ServerChecker.lua
+++
@@ -1,4 +0,0 @@
-function ModuleKick(args, sender)
-	if IsValid(sender) then sender:Kick("One or more modules have stopped working, please reconnect to fix.") end
-end
-Network:Subscribe("Modules_StoppedWorking", ModuleKick)
