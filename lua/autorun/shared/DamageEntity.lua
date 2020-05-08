d5fa1a0e894a89397bd11a477b71603fa265e094
benank <terkolskie@gmail.com>
2020-05-08 14:08:29 -0700

    Revert "Add pkill"
    This reverts commit 27b96e6784f3368110b0341df519b9976d9aeb77.

--- lua/autorun/shared/DamageEntity.lua
+++ lua/autorun/shared/DamageEntity.lua
@@ -9,7 +9,6 @@ DamageEntity.Thirst = 12
 DamageEntity.VehicleGuard = 13
 DamageEntity.WarpGrenade = 14
 DamageEntity.Suicide = 15
-DamageEntity.AdminKill = 16
 
 DamageEntityNames = 
 {
@@ -29,5 +28,4 @@ DamageEntityNames =
     [DamageEntity.VehicleGuard] = "Vehicle Guard",
     [DamageEntity.WarpGrenade] = "Warp Grenade",
     [DamageEntity.Suicide] = "Suicide",
-    [DamageEntity.AdminKill] = "Admin Kill",
 }
