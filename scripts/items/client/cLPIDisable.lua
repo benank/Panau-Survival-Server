ad85e80a958fab777077a04d74cf257900399e5e
benank <terkolskie@gmail.com>
2020-05-11 14:35:48 -0700

    Disable usage of health packs

--- scripts/items/client/cLPIDisable.lua
+++ scripts/items/client/cLPIDisable.lua
@@ -1,31 +1,7 @@
---[[local blacklist = {
-    [Action.NextWeapon] = true,
-    [Action.PrevWeapon] = true,
-    [Action.SwitchWeapon] = true,
-    [Action.Weapon0] = true,
-    [Action.Weapon1] = true,
-    [Action.Weapon2] = true,
-    [Action.Weapon3] = true,
-    [Action.Weapon4] = true,
-    [Action.Weapon5] = true,
-    [Action.Weapon6] = true,
-    [Action.Weapon7] = true,
-    [Action.Weapon8] = true,
-    [Action.Weapon9] = true,
-    [Action.SequenceButton1] = true,
-    [Action.SequenceButton2] = true,
-    [Action.SequenceButton3] = true,
-    [Action.SequenceButton4] = true,
-    [Action.EquipBlackMarketBeacon] = true,
-    [Action.EquipDual] = true,
-    [Action.EquipExplosive] = true,
-    [Action.EquipLeftSlot] = true,
-    [Action.EquipRightSlot] = true,
-    [Action.EquipTwohanded] = true
+local blacklist = {
+    [Action.UseItem] = true,
 }
 
 Events:Subscribe("LocalPlayerInput", function(args)
-
     if blacklist[args.input] then return false end
-
-end)]]
+end)
