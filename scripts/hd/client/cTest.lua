519b586083d3cc88d34100af276f0296d1561918
benank <terkolskie@gmail.com>
2020-05-23 16:14:09 -0700

    Add test to npc command

--- scripts/hd/client/cTest.lua
+++ scripts/hd/client/cTest.lua
@@ -1,5 +1,7 @@
-Events:Subscribe("LocalPlayerChat", function(args)
-    if args.text == "/npc" then
-        local npc = ClientActor.Create(AssetLocation.Game, {model_id = 39, position = LocalPlayer:GetPosition(), angle = Angle()})
-    end
-end)
+if IsTest then
+    Events:Subscribe("LocalPlayerChat", function(args)
+        if args.text == "/npc" then
+            local npc = ClientActor.Create(AssetLocation.Game, {model_id = 39, position = LocalPlayer:GetPosition(), angle = Angle()})
+        end
+    end)
+end
