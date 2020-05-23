714e658a4e25cf1c4ca1043bdda66182dd02edd8
benank <terkolskie@gmail.com>
2020-05-23 16:35:25 -0700

    Revert "Merge branch 'hitdetection-v2' into master"
    This reverts commit f482e141de12ac4448eed0e9d8bf640132791d99, reversing
    changes made to d59c8c5a8423e49d8027e9d041c550eb8d3eadb1.

--- scripts/hd/client/cTest.lua
+++
@@ -1,5 +0,0 @@
-Events:Subscribe("LocalPlayerChat", function(args)
-    if args.text == "/npc" then
-        local npc = ClientActor.Create(AssetLocation.Game, {model_id = 39, position = LocalPlayer:GetPosition(), angle = Angle()})
-    end
-end)
