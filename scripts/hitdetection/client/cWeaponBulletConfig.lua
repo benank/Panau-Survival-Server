7f60c01f0f1dcf50a58fbc25463e07d917cd5ffa
benank <terkolskie@gmail.com>
2020-06-07 13:43:47 -0700

    Adjust bullet travel times

--- scripts/hitdetection/client/cWeaponBulletConfig.lua
+++ scripts/hitdetection/client/cWeaponBulletConfig.lua
@@ -6,21 +6,21 @@ function cWeaponBulletConfig:__init()
         [WeaponEnum.MachineGun] = 
         {
             type = ProjectileBullet,
-            speed = 400,
+            speed = 600,
             bloom = 1.5,
             bullet_size = 0.5 -- Size of visual bullet only
         },
         [WeaponEnum.Handgun] = 
         {
             type = ProjectileBullet,
-            speed = 350,
+            speed = 550,
             bloom = 1,
             bullet_size = 0.1
         },
         [WeaponEnum.Assault] = 
         {
             type = ProjectileBullet,
-            speed = 350,
+            speed = 550,
             bloom = 1.5,
             bullet_size = 0.4 -- Size of visual bullet only
         },
@@ -42,7 +42,7 @@ function cWeaponBulletConfig:__init()
         [WeaponEnum.Revolver] = 
         {
             type = ProjectileBullet,
-            speed = 300,
+            speed = 500,
             bloom = 5,
             bullet_size = 0.25
         },
@@ -57,21 +57,21 @@ function cWeaponBulletConfig:__init()
         [WeaponEnum.SMG] = 
         {
             type = ProjectileBullet,
-            speed = 500,
+            speed = 700,
             bloom = 0.75,
             bullet_size = 0.1
         },
         [WeaponEnum.Sniper] = 
         {
             type = ProjectileBullet,
-            speed = 750,
+            speed = 1000,
             bloom = 5,
             bullet_size = 2
         },
         [WeaponEnum.SawnOffShotgun] = 
         {
             type = ProjectileBullet,
-            speed = 200,
+            speed = 400,
             bloom = 3,
             bullet_size = 0.1,
             multi_shot = 3
@@ -79,7 +79,7 @@ function cWeaponBulletConfig:__init()
         [WeaponEnum.Shotgun] = 
         {
             type = ProjectileBullet,
-            speed = 200,
+            speed = 400,
             bloom = 2,
             bullet_size = 0.2,
             multi_shot = 3
@@ -88,7 +88,7 @@ function cWeaponBulletConfig:__init()
         [WeaponEnum.V_Minigun] = 
         {
             type = ProjectileBullet,
-            speed = 600,
+            speed = 800,
             bloom = 0.5,
             bullet_size = 0.2,
             indicator = true,
@@ -99,7 +99,7 @@ function cWeaponBulletConfig:__init()
         [WeaponEnum.V_Rockets] = 
         {
             type = ProjectileBullet,
-            speed = 300,
+            speed = 400,
             bloom = 0,
             bullet_size = 0,
             splash = true,
@@ -111,7 +111,7 @@ function cWeaponBulletConfig:__init()
         [WeaponEnum.V_Cannon] = 
         {
             type = ProjectileBullet,
-            speed = 300,
+            speed = 500,
             bloom = 5,
             bullet_size = 1.0,
             splash = true,
@@ -123,7 +123,7 @@ function cWeaponBulletConfig:__init()
         [WeaponEnum.V_MachineGun] = 
         {
             type = ProjectileBullet,
-            speed = 500,
+            speed = 700,
             bloom = 1,
             bullet_size = 0.5,
             angle = function(cam_angle, v_angle)
