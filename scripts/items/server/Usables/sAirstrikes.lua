51554cf13b0c186c29ca0fe1e02b181d23a5d74e
benank <terkolskie@gmail.com>
2020-06-14 17:30:14 -0700

    Add better message

--- scripts/items/server/Usables/sAirstrikes.lua
+++ scripts/items/server/Usables/sAirstrikes.lua
@@ -118,7 +118,7 @@ function sAirStrikes:UseItem(args)
     local last_time = args.player:GetValue("LastAirstrikeTime")
 
     if Server:GetElapsedSeconds() - last_time < self.cooldown then
-        Chat:Send(args.player, "You muse wait before using this!", Color.Red)
+        Chat:Send(args.player, string.format("You muse wait %.0f seconds before using this!", Server:GetElapsedSeconds() - last_time), Color.Red)
         return
     end
 
