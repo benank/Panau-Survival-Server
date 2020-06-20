26971918c5cca0913dc94becf20943b4825de6d2
benank <terkolskie@gmail.com>
2020-06-20 09:18:01 -0700

    More drone progress

--- scripts/drones/server/sDrone.lua
+++ scripts/drones/server/sDrone.lua
@@ -1,5 +1,77 @@
 class 'sDrone'
 
+local DRONE_ID = 0
+local function GetDroneId()
+    DRONE_ID = DRONE_ID + 1
+    return DRONE_ID
+end
+
 function sDrone:__init(args)
 
+    self.id = GetDroneId()
+    self.level = args.level
+    self.position = args.position -- Approximate position of the drone in the world
+
+    self.target_position = self.position -- 
+    self.target = nil -- Current active target that the drone is pursuing
+    self.target_offset = Vector3() -- Offset from the target position the drone flies at
+
+    self.config = GetDroneConfiguration(self.level)
+
+    self.max_health = self.config.health
+    self.health = self.max_health
+
+    self.state = DroneState.Wandering
+    self.personality = self.config.attack_on_sight and DronePersonality.Hostile or DronePersonality.Defensive
+
+    self.controller = nil -- Player who currently "controls" the drone and dictates its pathfinding
+
+end
+
+-- Called when a player destroys a drone
+function sDrone:Destroyed(args)
+    -- Give args.player exp, set it to destroyed, sync to players
+
+    self.state = DroneState.Destroyed
+
+    self:Sync()
+end
+
+function sDrone:Damage(args)
+
+    self.health = math.max(0, self.health - args.damage)
+
+    self:Sync()
+
+    if self.health == 0 then
+        self:Destroyed(args)
+    end
+
+end
+
+-- Syncs the drone to a specified player, or if none specified, all players in nearby cells
+function sDrone:Sync(player)
+
+    if IsValid(player) then
+        Network:Send(player, "Drones/SingleSync", self:GetSyncData())
+    else
+        local nearby_players = sDroneManager:GetNearbyPlayersInCell(GetCell(self.position, Cell_Size))
+        Network:SendToPlayers(nearby_players, "Drones/SingleSync", self:GetSyncData())
+    end
+
+end
+
+function sDrone:GetSyncData()
+
+    return {
+        id = self.id,
+        position = self.position,
+        level = self.level,
+        config = self.config
+    }
+
+end
+
+function sDrone:ToString()
+    return string.format("")
 end
--- scripts/drones/server/sDroneManager.lua
+++ scripts/drones/server/sDroneManager.lua
@@ -2,15 +2,71 @@ class 'sDroneManager'
 
 function sDroneManager:__init()
 
+    self.drones = {} -- Drones in cells [x][y][id] = drone
+    self.player_cells = {} -- Players in cells [x][y][steam_id] = player
 
     Events:Subscribe("Cells/PlayerCellUpdate" .. tostring(Cell_Size), self, self.PlayerCellUpdate)
 
 end
 
+-- Updates LootCells.Player
+function sDroneManager:UpdatePlayerInCell(args)
+    local cell = args.cell
+
+    if args.old_cell.x ~= nil and args.old_cell.y ~= nil then
+        VerifyCellExists(self.player_cells, args.old_cell)
+        self.player_cells[args.old_cell.x][args.old_cell.y][tostring(args.player:GetSteamId().id)] = nil
+    end
+
+    VerifyCellExists(self.player_cells, cell)
+    self.player_cells[cell.x][cell.y][tostring(args.player:GetSteamId().id)] = args.player
+
+end
+
+function sDroneManager:GetNearbyPlayersInCell(cell)
+
+    local nearby_players = {}
+
+    -- Sync to all players in adjacent cells
+    for x = cell.x - 1, cell.x + 1 do
+
+        for y = cell.y - 1, cell.y + 1 do
+
+            VerifyCellExists(self.player_cells, {x = x, y = y})
+            for _, player in pairs(self.player_cells[x][y]) do
+
+                if IsValid(player) then
+                    table.insert(nearby_players, player)
+                end
+
+            end
+
+        end
+
+    end
+
+    return nearby_players
+
+end
+
 function sDroneManager:PlayerCellUpdate(args)
 
-    -- Update player cell and sync them new drones in new cells :)
+    self:UpdatePlayerInCell(args)
+    
+    local drone_data = {}
 
+    for _, update_cell in pairs(args.updated) do
+
+        -- If these cells don't exist, create them
+        VerifyCellExists(self.drones, update_cell)
+
+        for id, drone in pairs(self.drones[update_cell.x][update_cell.y]) do
+            table.insert(drone_data, drone:GetSyncData())
+        end
+    end
+    
+	-- send the existing lootboxes in the newly streamed cells
+    Network:Send(args.player, "Drones/DroneCellsSync", {drone_data = drone_data})
 end
 
 sDroneManager = sDroneManager()
