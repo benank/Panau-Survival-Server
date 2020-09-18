e2eeee0c616bb9dce52d85cab938c68916838ed4
benank <terkolskie@gmail.com>
2020-09-18 11:04:38 -0700

    Add perk req

--- scripts/items/server/Usables/sLandClaimPlacer.lua
+++ scripts/items/server/Usables/sLandClaimPlacer.lua
@@ -2,6 +2,7 @@ class 'sLandClaimPlacer'
 
 function sLandClaimPlacer:__init()
 
+    self.use_perk_req = 34
     Events:Subscribe("Inventory/UseItem", self, self.UseItem)
     Network:Subscribe("items/CancelLandclaimPlacement", self, self.CancelLandclaimPlacement)
 
@@ -22,6 +23,16 @@ function sLandClaimPlacer:UseItem(args)
     if args.player:InVehicle() then return end
 
     if not args.item.custom_data.size then return end
+
+    local perks = args.player:GetValue("Perks")
+
+    if not perks.unlocked_perks[self.use_perk_req] then
+        local perks_by_id = SharedObject.GetByName("ExpPerksById"):GetValue("Perks")
+        Chat:Send(args.player, 
+            string.format("You must unlock the LandClaim perk (#%d) in order to use this. Hit F2 to open the perks menu.", 
+            perks_by_id[self.use_perk_req].position), Color.Red)
+        return
+    end
 
     if args.player:GetValue("StuntingVehicle") then
         Chat:Send(args.player, "You cannot use this item while stunting on a vehicle!", Color.Red)
