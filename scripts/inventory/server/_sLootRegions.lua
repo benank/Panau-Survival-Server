9f89a9efd16bdf529098783958b5264e48a55469
benank <terkolskie@gmail.com>
2020-10-28 19:28:07 -0700

    Loot regions wip

---
+++ scripts/inventory/server/_sLootRegions.lua
@@ -0,0 +1,26 @@
+LootRegions = 
+{
+    CapeCarnival = 1
+}
+
+ItemsInLootRegions = 
+{
+    [LootRegions.CapeCarnival] = 
+    {
+        [Lootbox.Types.Level4] = 
+        {
+            ["EXPLOSIVE"] = 
+            {
+                ["EMP"] = {rarity = 0.05, min = 1, max = 1}
+            },
+            ["TOOL"] = 
+            {
+                ["Ping"] = {rarity = 0.3, min = 1, max = 2},
+            },
+            ["GEAR"] = 
+            {
+                ["Player Radar"] = {rarity = 0.01, min = 1, max = 1},
+            }
+        }
+    }
+}
