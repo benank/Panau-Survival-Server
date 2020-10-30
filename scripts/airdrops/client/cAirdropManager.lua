ed6a70f51c72bd55c7849353f80e287a271e0662
benank <terkolskie@gmail.com>
2020-10-30 11:56:12 -0700

    Add airdrop types and points

--- scripts/airdrops/client/cAirdropManager.lua
+++ scripts/airdrops/client/cAirdropManager.lua
@@ -2,6 +2,25 @@ class 'cAirdropManager'
 
 function cAirdropManager:__init()
 
+    if IsTest then
+        self.locations = {}
+        Events:Subscribe("LocalPlayerChat", self, self.LocalPlayerChat)
+    end
+
+end
+
+function cAirdropManager:LocalPlayerChat(args)
+    if args.text == "/loc" then
+        table.insert(self.locations, LocalPlayer:GetPosition())
+        Chat:Print("Saved location", Color.LawnGreen)
+    elseif args.text == "/printloc" then
+        print("---------------------")
+        for _, pos in pairs(self.locations) do
+            print(string.format("{x = %.3f, y = %.3f, z = %.3f},", pos.x, pos.y, pos.z))
+        end
+        print("---------------------")
+        Chat:Print("Printed all locations", Color.LawnGreen)
+    end
 end
 
 cAirdropManager = cAirdropManager()
---
+++ scripts/airdrops/shared/sAirdropTypes.lua
@@ -0,0 +1,6 @@
+AirdropType = 
+{
+    Low = 1, -- Low level airdrops, not very good
+    Mid = 2, -- Mid level airdrops, decent stuff
+    High = 3 -- High level airdrops, very good stuff
+}
