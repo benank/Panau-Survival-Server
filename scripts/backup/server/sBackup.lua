8ebdf1f6dda7d4f88d0282d6a2806e6bc4eb5997
benank <terkolskie@gmail.com>
2021-04-12 11:35:52 -0700

    Fix backup error

--- scripts/backup/server/sBackup.lua
+++ scripts/backup/server/sBackup.lua
@@ -1,14 +1,15 @@
-Timer.SetInterval(1000 * 60 * 60 * 24, function()
-    CreateBackup()
-end)
-
-Events:Subscribe("ModuleLoad", CreateBackup)
-
 function CreateBackup()
     print("Creating backup...")
     os.execute(string.format('sqlite3 server.db -cmd ".backup ./db_backups/%s.db" ".exit"', GetLogDate()))
     print("Backup created!")
 end
+
+
+Timer.SetInterval(1000 * 60 * 60 * 24, function()
+    CreateBackup()
+end)
+
+Events:Subscribe("ModuleLoad", CreateBackup)
 
 function GetLogDate()
 
