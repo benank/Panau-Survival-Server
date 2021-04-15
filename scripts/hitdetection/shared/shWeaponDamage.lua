d3f7fe9aa2048a6abb33df651a2c1a30a11d6488
benank <terkolskie@gmail.com>
2021-04-14 21:42:59 -0700

    Increase GL and RL damage

--- scripts/hitdetection/shared/shWeaponDamage.lua
+++ scripts/hitdetection/shared/shWeaponDamage.lua
@@ -12,9 +12,9 @@ function WeaponDamage:__init()
         [WeaponEnum.Handgun] =          {base = 0.09, v_mod = 0.05,  distance_falloff = 150, falloff = falloff_func},
         [WeaponEnum.Assault] =          {base = 0.09, v_mod = 0.08,  distance_falloff = 300, falloff = falloff_func},
         [WeaponEnum.BubbleGun] =        {base =-0.02, v_mod = 0,     distance_falloff = 50,  falloff = falloff_func},
-        [WeaponEnum.GrenadeLauncher] =  {base = 0.12, v_mod = 3,     distance_falloff = 0,   falloff = function() return 1 end, radius = 5},
+        [WeaponEnum.GrenadeLauncher] =  {base = 0.20, v_mod = 2,     distance_falloff = 0,   falloff = function() return 1 end, radius = 5},
         [WeaponEnum.Revolver] =         {base = 0.18, v_mod = 0.05,  distance_falloff = 300, falloff = falloff_func},
-        [WeaponEnum.RocketLauncher] =   {base = 0.15, v_mod = 4,     distance_falloff = 0,   falloff = function() return 1 end, radius = 4},
+        [WeaponEnum.RocketLauncher] =   {base = 0.40, v_mod = 2.5,   distance_falloff = 0,   falloff = function() return 1 end, radius = 4},
         [WeaponEnum.SMG] =              {base = 0.08, v_mod = 0.02,  distance_falloff = 100, falloff = falloff_func},
         [WeaponEnum.Sniper] =           {base = 0.90, v_mod = 0.04,  distance_falloff = 100, falloff = 
             function(distance, distance_falloff) -- Sniper gains full power at 100+ meters away
