a6f187f4ae99de0c54c90189eecd7182f1251f57
benank <terkolskie@gmail.com>
2021-04-17 09:13:28 -0700

    More work

--- scripts/quests/shared/_shQuestEnum.lua
+++ scripts/quests/shared/_shQuestEnum.lua
@@ -1,5 +1,6 @@
 QuestEnum = 
 {
+    Daily = 0,
     GettingStartedPart1 = 1,
     GettingStartedPart2 = 2,
     GettingStartedPart3 = 3
---
+++ scripts/quests/shared/shDailyQuests.lua
@@ -0,0 +1,21 @@
+DailyQuestTypes = 
+{
+    LootItem = 1, -- Loot specific item or X of specific item
+    UseItem = 2, -- Woet X or anything else
+    KillDrones = 2, -- Kill X number of drones (potentially high level)
+    VisitAreas = 3, -- 
+    Delivery = 4,
+    HiddenLockbox = 5, -- Spawn a locked box in an area and give clues to find it
+    LootArea = 6, -- Loot X boxes in area
+    LootAreaCompletely = 7, -- Loot all boxes in area
+    UseWorkbench = 8, -- Could be to combine specific items or get a 500% item
+    UseDifferentVehicles = 9, -- Use X different vehicles
+    TravelDistance = 10,
+    AchieveSpeed = 11, -- Get to X speed
+    HackSAMs = 12,
+    DestroySAMs = 13, -- potentially high level
+    DriveVehicle = 14, -- Drive X vehicle
+    LootAirdrop = 15, -- Loot an airdrop
+    LootBoxes = 16, -- Loot X t4 boxes, X t3 boxes, etc
+    StayOnline = 17, -- Stay online for X hours/minutes
+}
