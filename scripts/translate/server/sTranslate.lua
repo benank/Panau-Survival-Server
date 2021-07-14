dd63a3140db41fee8bc2aaea0906143019688048
benank <terkolskie@gmail.com>
2021-07-14 13:28:48 -0700

    Thanks to sinister for the fix!

--- scripts/translate/server/sTranslate.lua
+++ scripts/translate/server/sTranslate.lua
@@ -31,20 +31,62 @@ function send(content)
     assert(udp:sendto(tostring(content), ip, port))
 end
 
--- https://www.lua.org/pil/20.3.html
-function unescape(s)
-    s = string.gsub(s, "+", " ")
-    s = string.gsub(s, "%%(%x%x)", function (h)
-          return string.char(tonumber(h, 16))
-        end)
-    s = string.gsub(s, "%%u(%x%x%x)", function (h)
-            return string.char(tonumber(h, 16))
-        end)
-    s = string.gsub(s, "%%u(%x%x%x%x)", function (h)
-            return string.char(tonumber(h, 16))
-        end)
-    return s
+-- Thanks Sister_Rectus for the unescape code
+-----
+local str_char = string.char
+local band, bor, rshift = bit32.band, bit32.bor, bit32.rshift
+
+local function bin(str)
+  return tonumber(str, 2)
 end
+
+local codec = {
+  {0x00000000, 0x0000007F, bin('00000000'), bin('01111111')},
+  {0x00000080, 0x000007FF, bin('11000000'), bin('00011111')},
+  {0x00000800, 0x0000FFFF, bin('11100000'), bin('00001111')},
+  {0x00010000, 0x0010FFFF, bin('11110000'), bin('00000111')},
+}
+
+local mask = {bin('10000000'), bin('00111111')}
+
+local function range(n)
+  for i, v in ipairs(codec) do
+    if v[1] <= n and n <= v[2] then
+      return i
+    end
+  end
+  error('value out of range: ' .. n)
+end
+
+local function utf8char(n)
+  local i = range(n)
+  if i == 1 then
+    return str_char(n)
+  else
+    local buf = {}
+    for b = i, 2, -1 do
+      local byte = band(n, mask[2])
+      byte = bor(mask[1], byte)
+      buf[b] = str_char(byte)
+      n = rshift(n, 6)
+    end
+    n = bor(codec[i][3], n)
+    buf[1] = str_char(n)
+    return table.concat(buf)
+  end
+end
+
+local function unescape(s)
+  s = string.gsub(s, "+", " ")
+  s = string.gsub(s, "%%(%x%x)", function (h)
+    return utf8char(tonumber(h, 16))
+  end)
+  s = string.gsub(s, "%%u(%x%x%x%x)", function (h)
+    return utf8char(tonumber(h, 16))
+  end)
+  return s
+end
+-------
 
 -- ip, port, bytes, text
 function receive(text)
