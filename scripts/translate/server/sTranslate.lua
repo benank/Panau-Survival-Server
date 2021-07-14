e328ffe6eb962b79f46f7a01ff61751658c656c6
benank <terkolskie@gmail.com>
2021-07-14 12:58:29 -0700

    Test change

--- scripts/translate/server/sTranslate.lua
+++ scripts/translate/server/sTranslate.lua
@@ -31,30 +31,15 @@ function send(content)
     assert(udp:sendto(tostring(content), ip, port))
 end
 
--- http://lua-users.org/wiki/LuaUnicode
-function Utf8to32(utf8str)
-	assert(type(utf8str) == "string")
-	local res, seq, val = {}, 0, nil
-	for i = 1, #utf8str do
-		local c = string.byte(utf8str, i)
-		if seq == 0 then
-			table.insert(res, val)
-			seq = c < 0x80 and 1 or c < 0xE0 and 2 or c < 0xF0 and 3 or
-			      c < 0xF8 and 4 or --c < 0xFC and 5 or c < 0xFE and 6 or
-				  error("invalid UTF-8 character sequence")
-			val = bit32.band(c, 2^(8-seq) - 1)
-		else
-			val = bit32.bor(bit32.lshift(val, 6), bit32.band(c, 0x3F))
-		end
-		seq = seq - 1
-	end
-	table.insert(res, val)
-	table.insert(res, 0)
-	return res
-end
 
+
+-- https://www.lua.org/pil/20.3.html
 function unescape(s)
+    SetUnicode(true)
     s = string.gsub(s, "+", " ")
+    s = string.gsub(s, "%%u(%x%x%x%x)", function (h)
+            return string.char(tonumber(h, 16))
+        end)
     s = string.gsub(s, "%%(%x%x)", function (h)
           return string.char(tonumber(h, 16))
         end)
