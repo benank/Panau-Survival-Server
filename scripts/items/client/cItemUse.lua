d12bade2a7a7c4a267bb6559df09dfb443809ce0
benank <terkolskie@gmail.com>
2021-07-30 17:11:00 -0700

    Allow item use underwater

--- scripts/items/client/cItemUse.lua
+++ scripts/items/client/cItemUse.lua
@@ -58,7 +58,7 @@ function cItemUse:StartUsage(args)
     self.progress.max = args.time
     self.progress.current = 0
     Events:Fire("SetInventoryState", false)
-
+    
     table.insert(self.events, Events:Subscribe("InputPoll", self, self.InputPoll))
     table.insert(self.events, Events:Subscribe("Render", self, self.Render))
     table.insert(self.events, Events:Subscribe("LocalPlayerInput", self, self.LocalPlayerInput))
@@ -67,7 +67,8 @@ function cItemUse:StartUsage(args)
     table.insert(self.events, Events:Subscribe("LocalPlayerDeath", self, self.CancelUsage))
 
     if LocalPlayer:GetBaseState() ~= AnimationState.SUprightIdle
-    and LocalPlayer:GetBaseState() ~= AnimationState.SSwimIdle then
+    and LocalPlayer:GetBaseState() ~= AnimationState.SSwimIdle
+    and LocalPlayer:GetBaseState() ~= AnimationState.SSwimDiveIdle then
         self:CancelUsage()
     end
 
