6841fbf606162cba95b5bd2b4fa7876dd9038cf3
benank <terkolskie@gmail.com>
2021-08-06 17:34:24 -0700

    Update build limit to 6km

--- scripts/build/shared/shLandclaimObjectConfig.lua
+++ scripts/build/shared/shLandclaimObjectConfig.lua
@@ -1,6 +1,6 @@
 LandclaimObjectConfig = 
 {
     min_height = 0,
-    max_height = 8000,
+    max_height = 6000,
     decay_per_interval = 50 -- 10 health decay per decay interval to objects on expired claims
 }
