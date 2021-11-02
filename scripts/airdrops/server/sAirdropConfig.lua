0fd93191ce99b265e1d791c1a004afa7a347d032
benank <terkolskie@gmail.com>
2021-11-01 21:12:12 -0700

    Adjust airdrop spawning

--- scripts/airdrops/server/sAirdropConfig.lua
+++ scripts/airdrops/server/sAirdropConfig.lua
@@ -18,46 +18,46 @@ AirdropConfig =
     {
         [AirdropType.Low] = 
         {
-            min_players = 3,
+            min_players = 2,
             health = 2,
             drones = 
             {
                 amount = {min = 3, max = 8},
-                level = {min = 20, max = 30}
+                level = {min = 15, max = 30}
             },
             map_preview = -- Preview on discord and ingame map
             {
                 time = 10, -- How many minutes it appears on the map before it drops
                 size = 500 -- How big is the radius around it
             },
-            chance = 0.1, -- Chance of the airdrop spawning every interval if the conditions are met
+            chance = 0.2, -- Chance of the airdrop spawning every interval if the conditions are met
             interval = 60 -- 1 hour between airdrops of this type
         },
         [AirdropType.Mid] = 
         {
-            min_players = 5,
+            min_players = 3,
             health = 4,
             drones = 
             {
                 amount = {min = 4, max = 10},
-                level = {min = 30, max = 60}
+                level = {min = 30, max = 70}
             },
             map_preview = 
             {
                 time = 15,
                 size = 1000
             },
-            chance = 0.3,
+            chance = 0.5,
             interval = 120 -- 2 hours between airdrops
         },
         [AirdropType.High] = 
         {
-            min_players = 7,
+            min_players = 5,
             health = 6,
             drones = 
             {
-                amount = {min = 5, max = 10},
-                level = {min = 80, max = 120}
+                amount = {min = 5, max = 12},
+                level = {min = 80, max = 200}
             },
             map_preview = 
             {
