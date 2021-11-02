949b08f16644ab8e57e5a5fffc6fd3c517a2da52
benank <terkolskie@gmail.com>
2021-11-01 21:32:38 -0700

    Attempt to fix error

--- scripts/survival/client/cOxygen.lua
+++ scripts/survival/client/cOxygen.lua
@@ -2,13 +2,23 @@ local oxygen = var(0.5)
 local oxygen_per_sec_above = var(0.05)
 local oxygen_per_sec_below = var(-0.01)
 
+function GetOxygen()
+    local o2 = oxygen:get()
+    if o2 then
+        return tonumber(o2)
+    else
+        return 0.5
+    end
+end
+
 Network:Subscribe("Survival/UpdateOxygen", function(args)
     oxygen:set(args.oxygen)
 end)
 
+
 Events:Subscribe("Render", function(args)
     if LocalPlayer:GetHealth() > 0 and not LocalPlayer:GetValue("Loading") then
-        LocalPlayer:SetOxygen(tonumber(oxygen:get()))
+        LocalPlayer:SetOxygen(GetOxygen())
     else
         LocalPlayer:SetOxygen(1)
     end
@@ -18,7 +28,7 @@ Events:Subscribe("SecondTick", function()
     if LocalPlayer:GetValue("Loading") or LocalPlayer:GetHealth() <= 0 then return end
     
     local above_water = LocalPlayer:GetBonePosition("ragdoll_Head").y > 199
-    local current_oxygen = tonumber(oxygen:get())
+    local current_oxygen = GetOxygen()
     
     if above_water then
         oxygen:set(math.min(1, current_oxygen + tonumber(oxygen_per_sec_above:get())))
@@ -26,7 +36,7 @@ Events:Subscribe("SecondTick", function()
         oxygen:set(math.min(1, current_oxygen + tonumber(oxygen_per_sec_below:get())))
     end
     
-    if current_oxygen ~= tonumber(oxygen:get()) then
-        Network:Send("Survival/UpdateOxygen", {oxygen = tonumber(oxygen:get())})
+    if current_oxygen ~= GetOxygen() then
+        Network:Send("Survival/UpdateOxygen", {oxygen = GetOxygen()})
     end
 end)
