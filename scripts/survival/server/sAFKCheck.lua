40dcf060b1aa9052bacb82637922a341b34daadd
benank <terkolskie@gmail.com>
2022-04-27 17:28:06 -0700

    Make afk check more lenient

--- scripts/survival/server/sAFKCheck.lua
+++ scripts/survival/server/sAFKCheck.lua
@@ -1,5 +1,6 @@
 Events:Subscribe("ClientModuleLoad", function(args)
     args.player:SetValue("LastAFKTime", Server:GetElapsedSeconds())
+    args.player:SetValue("AFKTimerCount", 0)
 end)
 
 Events:Subscribe("MinuteTick", function()
@@ -11,15 +12,19 @@ Events:Subscribe("MinuteTick", function()
             local timeElapsed = currentTime - lastafktime
             
             if timeElapsed > 120 then
-                p:Kick("AFK timer")
+                p:SetValue("AFKTimerCount", p:GetValue("AFKTimerCount") + 1)
             elseif timeElapsed > 60 then
                 Network:Send(p, "AFKCheck")
             end
-        
+            
+            if p:GetValue("AFKTimerCount") > 5 then
+                p:Kick("AFK Timer")
+            end
        end
     end
 end)
 
 Network:Subscribe("AFKCheck", function(args, player)
     player:SetValue("LastAFKTime", Server:GetElapsedSeconds())
+    player:SetValue("AFKTimerCount", 0)
 end)
