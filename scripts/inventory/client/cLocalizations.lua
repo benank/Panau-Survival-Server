3f10fd4bfa80cc42a81c70e796bb8b566eabc0c0
benank <terkolskie@gmail.com>
2022-05-05 18:09:44 -0700

    Fix language switching back

--- scripts/inventory/client/cLocalizations.lua
+++ scripts/inventory/client/cLocalizations.lua
@@ -3,3 +3,15 @@ Network:Subscribe("Inventory/LocalizedItems", function(args)
     ClientInventory.ui:CreateWindow()
     ClientInventory.ui:RefreshInventoryDisplay()
 end)
+
+Events:Subscribe("NetworkObjectValueChange", function(args)
+    if args.object.__type ~= "Player" and args.object.__type ~= "LocalPlayer" then return end
+    if args.object ~= LocalPlayer then return end
+    if args.key ~= "Locale" then return end
+    
+    local locale = args.value
+    if LocalizedItemNames[locale] then
+        ClientInventory.ui:CreateWindow()
+        ClientInventory.ui:RefreshInventoryDisplay()
+    end
+end)
