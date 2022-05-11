ce422d8891e9eba44a45cd56e8a7ad4775fe9df8
benank <terkolskie@gmail.com>
2022-05-11 16:05:03 -0700

    Add another area

--- scripts/build/shared/shBlacklistedAreas.lua
+++ scripts/build/shared/shBlacklistedAreas.lua
@@ -18,6 +18,7 @@ end
 BlacklistedLandclaimAreas = 
 {
     {pos = Vector3(-10299, 204, -3012), size = 400},
+    {pos = Vector3(10839, 235, 9868), size = 500},
     -- Workbenches
     {pos = Vector3(2192.39, 649.05, 1365), size = 100},
     {pos = Vector3(-15314, 501.761, -2408.28), size = 100},
