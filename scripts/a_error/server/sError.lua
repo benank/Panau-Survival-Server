d4f877e0c4fe150d37298ade3719a254d935edad
benank <terkolskie@gmail.com>
2024-09-28 17:53:50 -0400

    Add error checks

--- scripts/a_error/server/sError.lua
+++ scripts/a_error/server/sError.lua
@@ -8,6 +8,12 @@ Events:Subscribe("ModuleError", function(e)
 end)
 
 Network:Subscribe("ModuleError", function(args, player)
+
+    if args.error.find("module build") then
+        player:Kick("Please restart your game in order to play on the server.")
+        print(string.format("Kicked player %s (%s) due to build errors", player:GetName(), player:GetSteamId()))
+    end
+
     Events:Fire("Discord", {
         channel = "Errors",
         content = tostring(args.error)
